ed9a436b0d89e1921b27c1a14966f5a4cf0c0bc3
selimdawa <selimdawa@gmail.com>
2026-08-29 06:59:46 +0300

    Edge to Edge - Add

--- app/src/main/java/com/flatcode/autoimageslider/MainActivity.kt
+++ app/src/main/java/com/flatcode/autoimageslider/MainActivity.kt
@@ -2,8 +2,12 @@ package com.flatcode.autoimageslider
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.flatcode.autoimageslider.databinding.ActivityMainBinding
 import io.selimdawa.autoimageslider.SliderAnimations
 import io.selimdawa.autoimageslider.SliderView
@@ -17,9 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("SliderApp", "MainActivity onCreate started")
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         adapter = SliderAdapterExample(this)
 
